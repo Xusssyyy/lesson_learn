130022834e0f0ef11b9100983990b92dfe6be2a5
Xsy <1006937132@qq.com>
2022-08-05 09:26:35 +0800

    同步异步async

---
+++ ryf/async/1.js
@@ -0,0 +1,5 @@
+//自上而下执行
+setTimeout(()=>{
+    console.log('timeout');
+},2000)
+console.log('1111');
---
+++ ryf/async/3.js
@@ -0,0 +1,31 @@
+// 事件 异步 
+// 订阅公众号？ 1000人 
+// setTimeout  addEventListener  订阅关系
+let eventEmiiter = {
+    list: {
+        'article': []
+    }, // 所有的订阅者
+    on(event, fn) {
+        // 订阅关系 数组？ 
+        this.list[event].push(fn);
+        // console.log(this.list[event]);
+    },
+    emit(event, title) {
+        this.list[event].forEach(fn => {
+            fn(title);
+        })
+    }
+};
+
+function user1(content) {
+    console.log('用户1订阅了', content)
+}
+function user2(content) {
+    console.log('用户2订阅了', content)
+}
+// 先订阅 再推送图文  异步
+// on 代表订阅
+eventEmiiter.on('article', user1);
+eventEmiiter.on('article', user2);
+// emit 代表发布内容
+eventEmiiter.emit('article', 'JavaScript 发布-订阅模式')
